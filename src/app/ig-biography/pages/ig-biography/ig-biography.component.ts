9de60812e74fe5aac96acddb52dc8586ed6d65f1
Edsonjsf <edsonjsf1990@gmail.com>
2022-12-28 11:38:10 -0400

    Update: now the content is more reactive and the copy btn works

--- src/app/ig-biography/pages/ig-biography/ig-biography.component.ts
+++ src/app/ig-biography/pages/ig-biography/ig-biography.component.ts
@@ -18,10 +18,33 @@ export class IgBiographyComponent implements OnInit {
 
   initForm(): FormGroup {
     return this.formBuilder.group({
-      miniBio: ['', Validators.maxLength(40)],
-      autoridad: ['', Validators.maxLength(35)],
-      descripcion: ['', Validators.maxLength(50)],
-      alerta: ['', Validators.maxLength(20)],
+      miniBio: ['', [Validators.maxLength(40)]],
+      autoridad: ['', [Validators.maxLength(35)]],
+      descripcion: ['', [Validators.maxLength(50)]],
+      alerta: ['', [Validators.maxLength(20)]],
     });
+  }
+
+  copiarAlPortapapeles() {
+    // Crea un campo de texto "oculto"
+    var aux = document.createElement('textarea');
+
+    // Asigna el contenido del elemento especificado al valor del campo
+    aux.innerHTML = `${this.igBiography.value.miniBio}
+${this.igBiography.value.autoridad}
+${this.igBiography.value.descripcion}
+${this.igBiography.value.alerta}`.trim();
+
+    // Añade el campo a la página
+    document.body.appendChild(aux);
+
+    // Selecciona el contenido del campo
+    aux.select();
+
+    // Copia el texto seleccionado
+    document.execCommand('copy');
+
+    // Elimina el campo de la página
+    document.body.removeChild(aux);
   }
 }
