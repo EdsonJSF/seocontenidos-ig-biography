63560ea63e75c8f7a22d42f1bd7b0107a3b2f58f
Edsonjsf <edsonjsf1990@gmail.com>
2022-12-30 10:34:54 -0400

    Updated: Form inputs with validators, maxlenth and lorem text emxamples

--- src/app/ig-biography/pages/ig-biography/ig-biography.component.ts
+++ src/app/ig-biography/pages/ig-biography/ig-biography.component.ts
@@ -8,20 +8,30 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class IgBiographyComponent implements OnInit {
   igBiography!: FormGroup;
+  _alertaLength: number = 150;
 
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
     this.igBiography = this.initForm();
-    console.log(this.igBiography);
+  }
+
+  public get alertaLength(): number {
+    this._alertaLength =
+      0 -
+      this.igBiography.get('miniBio')?.value.length -
+      this.igBiography.get('autoridad')?.value.length -
+      this.igBiography.get('descripcion')?.value.length;
+
+    return this._alertaLength;
   }
 
   initForm(): FormGroup {
     return this.formBuilder.group({
-      miniBio: ['', [Validators.maxLength(35)]],
-      autoridad: ['', [Validators.maxLength(25)]],
-      descripcion: ['', [Validators.maxLength(45)]],
-      alerta: ['', [Validators.maxLength(20)]],
+      miniBio: ['', [Validators.required, Validators.maxLength(35)]],
+      autoridad: ['', [Validators.required, Validators.maxLength(25)]],
+      descripcion: ['', [Validators.required, Validators.maxLength(45)]],
+      alerta: ['', [Validators.required, Validators.maxLength(20)]],
     });
   }
 }
