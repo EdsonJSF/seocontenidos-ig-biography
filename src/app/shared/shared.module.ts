9a78443f04a456d886f80722de543f0d1e2d3658
Edsonjsf <edsonjsf1990@gmail.com>
2022-12-30 10:55:02 -0400

    Toast component added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { NgbToastModule, NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { GoTopBtnComponent } from './components/go-top-btn/go-top-btn.component';
+import { ToastComponent } from './components/toast/toast.component';
 
-const importExport = [HeaderComponent, FooterComponent, GoTopBtnComponent];
+const importExport = [
+  HeaderComponent,
+  FooterComponent,
+  GoTopBtnComponent,
+  ToastComponent,
+];
 
 @NgModule({
   declarations: [importExport],
-  imports: [CommonModule],
+  imports: [CommonModule, NgbTooltipModule, NgbToastModule],
   exports: importExport,
 })
 export class SharedModule {}
